b04ede057e389daa9a8d541f4a41b63f061777cb
Asteway Nigussie <asteway17@gmail.com>
2025-05-25 18:14:34 -0500

    declare variables, add constructor, add getters

--- src/main/java/com/pluralsight/models/Order.java
+++ src/main/java/com/pluralsight/models/Order.java
@@ -1,4 +1,34 @@
 package com.pluralsight.models;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+
+    //
+    private String orderNumber;
+    private LocalDateTime orderDateTime;
+    private List<Sandwich> sandwiches;
+    private List<Drinks> drinks;
+    private List<Chips> chips;
+
+    //
+    private static int orderCounter = 0;
+
+    //
+    public Order() {
+        this.orderNumber = "ORDER NUMBER - " + (++orderCounter); //
+        this.orderDateTime = LocalDateTime.now();
+        this.sandwiches = new ArrayList<>();
+        this.drinks = new ArrayList<>();
+        this.chips = new ArrayList<>();
+    }
+
+    //
+    public String getOrderNumber() { return orderNumber; }
+    public LocalDateTime getOrderDateTime() { return orderDateTime; }
+    public List<Sandwich> getSandwiches() { return sandwiches; }
+    public List<Drinks> getDrinks() { return drinks; }
+    public List<Chips> getChips() { return chips; }
 }
