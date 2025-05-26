ce8f1a92dac0f71f941415163ac84d6cc8c956b3
Asteway Nigussie <asteway17@gmail.com>
2025-05-26 15:28:59 -0500

    add the receipt writer saveReceipt method

--- src/main/java/com/pluralsight/utilities/ReceiptWriter.java
+++ src/main/java/com/pluralsight/utilities/ReceiptWriter.java
@@ -1,4 +1,33 @@
 package com.pluralsight.utilities;
 
+import com.pluralsight.models.Order;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class ReceiptWriter {
+
+    //
+    public static void saveReceipt(Order order) {
+        try {
+            //
+            DateTimeFormatter fileNameFormatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+            //
+            FileWriter fileWriteBoi = new FileWriter("src/main/resources/receipts/"
+                    + LocalDateTime.now().format(fileNameFormatter) + ".txt");
+            BufferedWriter buffWriteBoi = new BufferedWriter(fileWriteBoi);
+            buffWriteBoi.write(receiptFormatter(order));
+            buffWriteBoi.close();
+        } catch (IOException e) {
+            System.err.println("Error! Could not write to file: " + e.getMessage());
+        }
+    }
+
+    //
+    public static String receiptFormatter(Order order) {
+
+    }
 }
