84ce84ad6f3dc7aabf59ef88e4b601ef2da0f688
Asteway Nigussie <asteway17@gmail.com>
2025-05-26 14:20:57 -0500

    decalare variables, constructor, getters

--- src/main/java/com/pluralsight/models/Drink.java
+++ src/main/java/com/pluralsight/models/Drink.java
@@ -1,4 +1,31 @@
 package com.pluralsight.models;
 
 public class Drink {
+
+    //
+    private String size;
+    private String flavor;
+    private double price;
+
+    //
+    public Drink(String size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+        this.price = priceForSize();
+    }
+
+    //
+    private double priceForSize() {
+
+    }
+
+    //
+    public String getSummary() {
+        return String.format("%s %s - $%.2f", size, flavor, price);
+    }
+
+    //
+    public String getSize() { return size; }
+    public String getFlavor() { return flavor; }
+    public double getPrice() { return price; }
 }
